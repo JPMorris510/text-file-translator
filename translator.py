6213a23c2ece9df80ef262e7566912353336a4b6
JPMorris510 <55984158+JPMorris510@users.noreply.github.com>
2022-04-25 11:03:08 +0100

    Uploaded base code

---
+++ translator.py
@@ -0,0 +1,19 @@
+#Create a file and insert the provided text
+
+
+#Read the content of the file to the console
+
+
+#Append the other half of the text to the file
+
+
+#Create two more files using the same method for the translations of the text
+#Spanish
+
+#Swedish
+
+#Program a function which allows the user to select the language for the text they want to view
+
+lang = input("Select a language: English(en), Spanish(es) or Swedish(se)")
+
+#Program a function that allows a user to add more information to the file
